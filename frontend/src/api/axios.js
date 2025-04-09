3a40ebccf6f5608c0082cc688252f64c967e38da
ady7ady7 <adrianoovv2@gmail.com>
2025-04-09 14:35:51 +0200

    akt

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
+// frontend/src/api/axios.js
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
